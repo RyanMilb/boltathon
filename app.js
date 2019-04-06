1739bf6e831445f92b930691105081f1540942b9
RyanMilb <ryan@nostra.network>
2019-04-06 08:46:33 +0700

    remove junk

--- app.js
+++ app.js
@@ -7,53 +7,11 @@ const path = require('path')
 const text = fs.readFileSync(path.resolve(__dirname, 'output6.txt'), 'utf8')
 
 let T = new Twitter(config);
-// console.log(text)
 
-
-
-
-// const body = {
-//     userid: 8085,//pullUserId(res),
-//     username: 'Prediction_Feed',//userName,
-//     istaco: 0,
-//     tweet: 1099212914022440961,
-// };
-
-// console.log(body)
-// fetch("https://tippin.me/lndreq/newinvoice.php",
-//     {
-//         "credentials": "include", "headers":
-//             { "accept": "application/json, text/javascript, */*; q=0.01", "accept-language": "en-US,en;q=0.9", "content-type": "application/x-www-form-urlencoded; charset=UTF-8", "x-requested-with": "XMLHttpRequest" }
-//             , "referrer": "https://tippin.me/buttons/send-lite.php?u=Prediction_Feed&eh=yes&t=1099212914022440961", "referrerPolicy": "no-referrer-when-downgrade", "body": "userid=8058&username=Prediction_Feed&istaco=0&tweet=1099212914022440961", "method": "POST", "mode": "cors"
-//     })
-//     // fetch('https://tippin.me/lndreq/newinvoice.php', {
-//     //     method: 'post',
-//     //     body: JSON.stringify(body),
-//     //     headers: { 'Content-Type': 'application/json' },
-//     // })
-//     .then(res => res.json())
-//     .then(json => console.log(json));
-
-
-
-
-// const globalUserId = pullUserId(text);
-// console.log('Regex Test for user ID: ' + globalUserId)
-// Set up your search parameters
 async function testFetch() {
     console.log('Starting Fetch Test')
-
-    // fetch("https://tippin.me/buttons/send-lite.php?u=LightningK0ala&eh=yes&t=1098692883056652288", {"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","cache-control":"max-age=0","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"})
     fetch("https://tippin.me/buttons/send-lite.php?u=LightningK0ala&eh=yes&t=1098692883056652288", {"credentials":"include","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","accept-language":"en-US,en;q=0.9","cache-control":"max-age=0","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"})
-    // await fetch("https://api.tippin.me/@ryanmilb", {
-    //     body: "t=0&u=0",
-    //     headers: {
-    //         Authorization: "basic YjZiNjBjYWU0MDlkZTY3OWNjN2IxMDA3NjMzODdkZmE6MDI2ZTdhNWQ5ZDI1MTkzYzNkYWRmNzExOWIzYzliZGQK",
-    //         "Content-Type": "text/plain;charset=UTF-8"
-    //     },
-    //     method: "POST"
-    // }
-    // )
+
     .then(res => {
         console.log('res: ' + JSON.stringify(res))
         console.log('pulled this: ' + pullUserIdFromResponse(res))
