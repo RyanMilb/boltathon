a61123391bb9b1d43ec3476cdd533cf49e02de8f
Michael <michael@runcrypto.com>
2019-04-07 14:46:02 +0000

    genrating keys and signing nearly complete

--- signingServer.js
+++ signingServer.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var routes = require("./routes/routes.js");
 var app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const fs = require('fs');
+const crypto = require('crypto');
 const { randomBytes } = require('crypto');
 const secp256k1 = require('secp256k1');
 
@@ -19,57 +19,72 @@ const privFile = keyName + '.key';
 const pubFile = keyName + '.pub';
 
 const enc = 'utf-8';
+const hashAlgo = 'md5';
 
-var privateKey, publicKey;
+async function getPrivateKey() {
+    //
+    // try and open the private key file
+    //
+    var privateKey;
 
-// 
-// MANAGE LOCAL KEYS
-//
-// try and open the private key file
-//
-try {
+    try {
 
-    privateKey = fs.readFileSync(privFile, enc);
+        privateKey = fs.readFileSync(privFile, enc);
 
-} catch (e) {
+    } catch (e) {
 
-    // create keys if not present
-    console.log("Could not find keyfile: " + e);
-    // generate privKey
-    let privKey
-    do {
-        privKey = randomBytes(32)
-    } while (!secp256k1.privateKeyVerify(privKey));
-    console.log("generated keyfile");
-    privateKey = privKey;
+        // create keys if not present
+        console.log("Could not find keyfile: " + e);
+        // generate privKey
+        let privKey
+        do {
+            privateKey = randomBytes(32)
+        } while (!secp256k1.privateKeyVerify(privateKey));
 
-    // write keyfile
-    fs.write(privFile,privKey,enc);
+        console.log("generated keyfile");
 
-    // generate public key
-    genPubKey(privKey);
+        // write keyfile
+        fs.writeFileSync(privFile,privateKey,enc);
+
+    }
+
+    var buf = Buffer.from(privateKey);
+
+    // delete existing public key
+    try {
+    	fs.unlinkSync(pubFile);
+    } catch (e) {
+        console.log("public key does not exist");
+    }
+
+    return buf;
 }
 
-// load the public key from disk.  This is separate as the private
-// key may be on disk but not the pub key (for some reason) and we don't
-// want to regenerate the private key un-nesseccarily
-try {
+async function getPublicKey() {
+    // load the public key from disk.  This is separate as the private
+    // key may be on disk but not the pub key (for some reason) and we don't
+    // want to regenerate the private key un-nesseccarily
 
-    publicKey = fs.readFileSync(pubFile, enc);
+    var publicKey;
 
-} catch (e) {
+    try {
 
-    console.log("public keyfile not found: " + e );
-    publicKey = genPubKey(privateKey);
+        publicKey = fs.readFileSync(pubFile, enc);
 
-}
+    } catch (e) {
 
-function genPubKey(privKey){
-    // get the public key in a compressed format
-    pubKey = secp256k1.publicKeyCreate(privKey);
-    console.log("generated public key");
-    fs.write(pubFile,pubKey,enc);
-    return pubKey;
+        console.log("public keyfile not found: " + e );
+        var privateKey = await getPrivateKey();
+        //generate public key
+        publicKey = secp256k1.publicKeyCreate(privateKey);
+        console.log("generated public key");
+        //write public key to disk
+        fs.writeFileSync(pubFile,publicKey,enc);
+
+    }
+
+    return Buffer.from(publicKey);
+
 }
 
 //
@@ -81,44 +96,52 @@ var server = app.listen(3001, function () {
 
 
 //
-// return the public key used for signing
-//
-async function getPubKey() {
-    var fileContents = fs.readFileSync('keys/tls.cert', 'utf8');
-    console.log(fileContents);
-    return fileContents;
-}
-
-//
 // sign a message using the local private key
 //
-async function sign(message,privateKey) {
+async function sign(message) {
     // was going to use lncli but verifymessage won't work as you can't specify the public key when verifying message
-
+    console.log("message: " + message);
+    var h = hash(message);
+    var buf = Buffer.from(h);
+    // read the private key from file
+    var privateKey = await getPrivateKey();
     // sign the message
-    sigObj = secp256k1.sign(message, privateKey)
-    console.log('signature:' + sigObj.signature);
-    return sigObj.signature;
+    sigObj = secp256k1.sign(buf, privateKey);
+    signature = sigObj.signature.toString();
+    console.log('signature:' + signature);
+    return signature;
 }
 
+//
+// given a message, signature and public key verify that the signature is valid
+//
 async function verify(message,signature,pubKey) {
-    result = secp256k1.verify(message,signature,pubKey);
+    var h = hash(message);
+    result = secp256k1.verify(h,signature,pubKey);
     console.log('result from verify : ' + result);
     return result;
 }
 
+function hash(message) {
+    // need to hash the message down to 32 characters
+    var h = crypto.createHash(hashAlgo).update(message).digest('hex');
+    console.log("message hash: " + h);
+    return h;
+}
+
 app.post('/getSignature', async function(request, response){
     // read message from post data
-    let message = request.body.message;
-    console.log(request.body);
+    let message = JSON.stringify(request.body);
+    console.log(message);
     // sign the message
-    signature = sign(message);
+    var h = hash(message);
+    signature = sign(h);
     // get the public key associated with the signature
-    pubKey = getPubKey();
+    pubKey = getPublicKey();
     // create JSON response
     var jsondata = {
 	"message" : message,
-	"type" : "sha256",
+	"type" : hashAlgo,
         "publicKey" : pubKey,
 	"signature" : signature,
     };
@@ -128,11 +151,19 @@ app.post('/getSignature', async function(request, response){
 
 app.post('/verify', async function(request, response){
     // read message from post data
-    let message = request.body.message;
+    let message = Buffer.from(request.body.message);
     let signature = request.body.signature;
     let key = request.body.key;
     console.log(request.body);
-    verify(message,signature,key);
+    var jsondata = {
+      "message" : message,
+      "publicKey" : key,
+      "type" : hashAlgo,
+      "signature" : signature,
+      "isValid" : verify(message,signature,key)
+    };
+    response.send(jsondata);
 });
 
   //curl -vX POST localhost:3001/getCredential -d @tempExampleTemplate.json --header "Content-Type: application/json"
+  //curl -vX POST 139.59.213.116:3001/getSignature -d @tempExampleTemplate.json --header "Content-Type: application/json"
